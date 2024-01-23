b6df18b0a855d6a7a3b2d23e70a61c8cf5f53535
Jared De Blander <jared0x90@gmail.com>
2024-01-22 23:55:07 -0500

    updates to game.py

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+import os
 import pygame
 from scene import Scene
 from scenes.empty import Empty
@@ -6,27 +7,29 @@ from scenes.title import Title
 
 class Game:
     def __init__(self):
-        # set the quit flag
+        # set the quit flag to false at the start
         self.quit = False
 
+        # initialize pygame
         pygame.init()
 
         # create a window
         self.screen = pygame.display.set_mode((320, 180))
         pygame.display.set_caption("Jack's Quest")
+
+        # create a pygame clock to limit the game to 60 fps
         self.clock = pygame.time.Clock()
 
         # create a stack for scenes to be updated and drawn
+        # and add the title scene to the stack
         self.scene = []  # type: list[Scene]
+        self.scene_list = ["Title"]
+        self.scene.append(Title(self))
 
+        # create variables to handle scene changes
         self.scene_replace = None
         self.scene_push = None
         self.scene_pop = None
-
-        self.scene_list = ["Title"]
-
-        # add the title scene to the stack
-        self.scene.append(Title(self))
 
     def run(self):
         while not self.quit:
@@ -40,22 +43,23 @@ class Game:
             for scene in self.scene:
                 scene.draw()
 
-            # process scene change requests (if any)
-            self.change_scenes()
-
             # update the display
             pygame.display.flip()
+
+            # process scene change requests (if any)
+            self.change_scenes()
 
             # limit the game to 60 fps
             self.clock.tick(60)
 
     def process_events(self):
+        self.pressed = pygame.key.get_pressed()
+        self.just_pressed = []
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.quit = True
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    self.quit = True
+            elif event.type == pygame.KEYDOWN:
+                self.just_pressed.append(event.key)
 
     def load_asset(self, asset_path: str):
         return pygame.image.load("assets/" + asset_path).convert_alpha()
@@ -87,3 +91,19 @@ class Game:
             return eval(scene + "(self)")
         else:
             return Title(self)
+
+    # from the pygame tutorial:
+    # https://www.pygame.org/docs/tut/tom_games3.html
+    def load_png(self, name):
+        """Load image and return image object"""
+        fullname = os.path.join("data", name)
+        try:
+            image = pygame.image.load(fullname)
+            if image.get_alpha() is None:
+                image = image.convert()
+            else:
+                image = image.convert_alpha()
+        except FileNotFoundError:
+            print(f"Cannot load image: {fullname}")
+            raise SystemExit
+        return image, image.get_rect()
